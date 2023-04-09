168d2ecbde0a943a2782f12c1d066b63eda4888d
Jacob Horton <jacob.horton@ku.edu>
2023-04-09 13:13:49 -0500

    Implemented Functions

--- buddy.c
+++ buddy.c
@@ -50,7 +50,9 @@
  **************************************************************************/
 typedef struct {
 	struct list_head list;
-	/* TODO: DECLARE NECESSARY MEMBER VARIABLES */
+	int order;
+	int index;
+	void* memory;
 } page_t;
 
 /**************************************************************************
@@ -68,7 +70,9 @@ page_t g_pages[(1<<MAX_ORDER)/PAGE_SIZE];
 /**************************************************************************
  * Public Function Prototypes
  **************************************************************************/
+int get_order(int x);
 
+void split_page(page_t* page, int i, int order);
 /**************************************************************************
  * Local Functions
  **************************************************************************/
@@ -80,12 +84,14 @@ void buddy_init()
 {
 	int i;
 	int n_pages = (1<<MAX_ORDER) / PAGE_SIZE;
-	for (i = 0; i < n_pages; i++) {
-		/* TODO: INITIALIZE PAGE STRUCTURES */
+	for (i = 0; i < n_pages; i++){
+		g_pages[i].order = -1;
+		g_pages[i].index = i;
+		g_pages[i].memory = PAGE_TO_ADDR(i);
 	}
 
 	/* initialize freelist */
-	for (i = MIN_ORDER; i <= MAX_ORDER; i++) {
+	for (i = MIN_ORDER; i <= MAX_ORDER; i++){
 		INIT_LIST_HEAD(&free_area[i]);
 	}
 
@@ -109,8 +115,46 @@ void buddy_init()
  */
 void *buddy_alloc(int size)
 {
-	/* TODO: IMPLEMENT THIS FUNCTION */
+	int order = get_order(size);
+	if (order != -1){
+		for (int i = order; i <= MAX_ORDER; i++){
+			if (!list_empty(&free_area[i])){
+				page_t* page = list_entry(free_area[i].next, page_t, list);
+				if( order == i){
+					list_del_init(&(page->list));
+					page->order = order;
+					return (page->memory);
+				}
+				else{
+					list_del_init(&(page->list));
+					split_page(page, i, order);
+					page->order = order;
+					return (page->memory);
+				}
+			}
+		}
+	}
 	return NULL;
+}
+
+/**
+ * Splits a page into two buddies recursively, until the desired order is reached.
+ * The function operates on the page and its buddies, which are represented as a 
+ * binary tree. The page is split into two buddies of half the size of the original 
+ * page, until the desired order is reached.
+ *
+ * @param page  pointer to the page to be split
+ * @param i     the order of the page
+ * @param order the desired order to split the page into
+ */
+void split_page(page_t* page, int i, int order){
+	if (i != order){
+		int new_page_order = i-1;
+		page_t* buddy_page = &g_pages[ADDR_TO_PAGE(BUDDY_ADDR(page->memory, new_page_order))];
+		buddy_page->order = new_page_order;
+		list_add(&(buddy_page->list), &free_area[new_page_order]);
+		split_page(page, new_page_order, order);
+	}
 }
 
 /**
@@ -124,7 +168,34 @@ void *buddy_alloc(int size)
  */
 void buddy_free(void *addr)
 {
-	/* TODO: IMPLEMENT THIS FUNCTION */
+	page_t* current_page = &g_pages[ADDR_TO_PAGE(addr)];
+	page_t* buddy_page = &g_pages[ADDR_TO_PAGE(BUDDY_ADDR(current_page->memory, current_page->order))];
+	struct list_head *current_node;
+	page_t* temp = NULL;
+	int index = current_page->index;
+	for (int i = current_page->order; i <= MAX_ORDER; i++){
+		current_node = NULL;
+		buddy_page = &g_pages[ADDR_TO_PAGE(BUDDY_ADDR(current_page->memory, i))];
+		list_for_each(current_node, &free_area[i]){
+			temp = list_entry(free_area[i].next, page_t, list);
+			if(current_node == NULL){
+				break;
+			}
+			if (list_entry(current_node, page_t, list) == buddy_page){
+				break;
+			}
+		}
+		if(current_node == NULL || temp != buddy_page){
+			g_pages[index].order = -1;
+			list_add(&g_pages[index].list, &free_area[i]);
+			return;
+		}
+		if (buddy_page->memory < current_page->memory){
+			current_page = buddy_page;
+			index = current_page->index;
+		}
+		list_del_init(&buddy_page->list);
+	}
 }
 
 /**
@@ -134,14 +205,31 @@ void buddy_free(void *addr)
  */
 void buddy_dump()
 {
-	int o;
-	for (o = MIN_ORDER; o <= MAX_ORDER; o++) {
-		struct list_head *pos;
-		int cnt = 0;
-		list_for_each(pos, &free_area[o]) {
-			cnt++;
+	int order_index;
+	for (order_index = MIN_ORDER; order_index <= MAX_ORDER; order_index++){
+		struct list_head *current_node;
+		int count = 0;
+		list_for_each(current_node, &free_area[order_index]){
+			count++;
 		}
-		printf("%d:%dK ", cnt, (1<<o)/1024);
+		printf("%d:%dK ", count, (1<<order_index)/1024);
 	}
 	printf("\n");
+}
+
+/**
+ * Returns the order of the smallest power of 2 that can fit a given size.
+ * The size is expressed in number of bytes.
+ * 
+ * @param x the size to be allocated, in bytes
+ * @return the order of the smallest power of 2 that can fit the given size, 
+ *         or -1 if the size is larger than the maximum supported order
+ */
+int get_order(int x){
+ 	for (int order = MIN_ORDER; order <= MAX_ORDER; order++){
+	 	if (1 << order >= x){
+		 	return order;
+	 	}
+ 	}
+ 	return -1;
 }
